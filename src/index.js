45d48b6b12e66420370c6a686f978cfd9d11c7dd
Fernando Pasik <fernando@pasik.com.ar>
2016-11-10 16:29:41 -0300

    feat: Children utils
    * Filter 1st level of children by predicate
    * Group by element type and return a map with all elements by types
    specified and the rest
    * Deep map, foreach and find children and its children
    * Get only text nodes from children object at any depth

---
+++ src/index.js
@@ -0,0 +1,113 @@
+import { Children, cloneElement } from 'react';
+
+export default {
+
+  ...Children,
+
+  /**
+   * Filter children
+   * @param   {object} children - React component children
+   * @param {function} filterFn - Array filter callback
+   * @returns  {array}          - Filtered children
+   */
+  filter(children, filterFn) {
+    return Children
+      .toArray(children)
+      .filter(filterFn);
+  },
+
+  /**
+   * Group children by type and puts in a rest key
+   * the types not indicated
+   * @param   {object} children - React component children
+   * @param {string[]} types    - Array of child types
+   * @param   {string} rest     - Object key name where non types will be saved
+   * @returns {object}          - Map of the types and rest
+   */
+  groupByType(children, types, rest) {
+    return Children
+      .toArray(children)
+      .reduce((group, child) => {
+        const
+          isGrouped = types.includes(child.type),
+          addChild = isGrouped ? child.props.children : child,
+          key = isGrouped ? child.type : rest;
+        return {
+          ...group,
+          [key]: [ ...(group[key] || []), addChild ]
+        };
+      }, {});
+  },
+
+  /**
+   * Map children and its children
+   * @param   {object} children  - React component children
+   * @param {function} deepMapFn - Deep Map callback
+   * @returns  {array}           - Deep Mapped children
+   */
+  deepMap(children, deepMapFn) {
+    return Children
+      .map(children, child => {
+        if (child.props && child.props.children
+          && typeof child.props.children === 'object') {
+          // Clone the child that has children and map them too
+          return deepMapFn(cloneElement(child, {
+            ...child.props,
+            children: this.deepMap(child.props.children, deepMapFn)
+          }));
+        }
+        return deepMapFn(child);
+      });
+  },
+
+  /**
+   * ForEach children and its children
+   * @param   {object} children      - React component children
+   * @param {function} deepForEachFn - Deep Map callback
+   */
+  deepForEach(children, deepForEachFn) {
+    Children
+      .forEach(children, child => {
+        if (child.props && child.props.children
+          && typeof child.props.children === 'object') {
+          // Each inside the child that has children
+          this.deepForEach(child.props.children, deepForEachFn);
+        }
+        deepForEachFn(child);
+      });
+  },
+
+  /**
+   * Find in children and its children
+   * @param   {object} children   - React component children
+   * @param {function} deepFindFn - Deep Map callback
+   * @returns  {array}            - Children found
+   */
+  deepFind(children, deepFindFn) {
+    return Children
+      .toArray(children)
+      .find(child => {
+        if (child.props && child.props.children
+          && typeof child.props.children === 'object') {
+          // Find inside the child that has children
+          return this.deepFind(child.props.children, deepFindFn);
+        }
+        return deepFindFn(child);
+      });
+  },
+
+  /**
+   * Get only the text in children and its children
+   * @param   {object} children - React component children
+   * @returns  {string}         - Text of all children
+   */
+  onlyText(children) {
+    return Children
+      .toArray(children)
+      .reduce((flattened, child) => [
+        ...flattened,
+        child.props && child.props.children ? this.onlyText(child.props.children) : child
+      ], [])
+      .join('');
+  }
+};
---
+++ test/index.spec.js
@@ -0,0 +1,88 @@
+import React, { cloneElement, PropTypes } from 'react';
+import Children from '../src/index.js';
+import { shallow } from 'enzyme';
+
+describe('Children', () => {
+
+  it('filter', () => {
+    const Filtered = props => <div>{ Children.filter(props.children, item => item.type === 'span') }</div>;
+    Filtered.propTypes = { children: PropTypes.node };
+    const wrapper = shallow(<Filtered><span>1</span><span>2</span><strong>3</strong></Filtered>);
+    expect(wrapper).to.have.exactly(2).descendants('span');
+    expect(wrapper).to.not.have.descendants('strong');
+  });
+
+  it('group by type', () => {
+    const Grouped = props => (
+      <div>
+        <div className="spans">{ Children.groupByType(props.children, [ 'span', 'i' ], 'rest').span }</div>
+        <div className="rest">{ Children.groupByType(props.children, [ 'span', 'i' ], 'rest').rest }</div>
+        <div className="empty">{ Children.groupByType(props.children, [ 'span', 'i' ], 'rest').i }</div>
+      </div>
+    );
+    Grouped.propTypes = { children: PropTypes.node };
+    const wrapper = shallow(
+      <Grouped><span><b>1</b></span><span><b>2</b></span><strong>3</strong></Grouped>
+    );
+    expect(wrapper.find('.spans')).to.have.exactly(2).descendants('b');
+    expect(wrapper.find('.spans')).to.not.have.descendants('strong');
+    expect(wrapper.find('.rest')).to.not.have.descendants('span');
+    expect(wrapper.find('.rest')).to.have.exactly(1).descendants('strong');
+    expect(wrapper.find('.empty')).to.be.blank();
+  });
+
+  it('deep map', () => {
+    const DeepMapped = props => (
+      <div>
+        { Children.deepMap(props.children,
+          child => child.type === 'b'
+            ? cloneElement(child, { ...child.props, className: 'mapped' })
+            : child
+        ) }
+      </div>
+    );
+    DeepMapped.propTypes = { children: PropTypes.node };
+    const wrapper = shallow(
+      <DeepMapped><b>1</b><b>2</b><span><b>3</b></span><div><div><b>4</b></div></div></DeepMapped>
+    );
+    expect(wrapper).to.have.exactly(4).descendants('.mapped');
+  });
+
+  it('deep each', () => {
+    const texts = [];
+    const DeepForEached = props => (
+      <div>
+        { Children.deepForEach(props.children, child => {
+          if (child.type === 'b') {
+            texts.push(child.props.children);
+          }
+        }) }
+      </div>
+    );
+    DeepForEached.propTypes = { children: PropTypes.node };
+    shallow(
+      <DeepForEached><b>1</b><b>2</b><span><b>3</b></span><div><div><b>4</b></div></div></DeepForEached>
+    );
+    expect(texts).to.eql([ '1', '2', '3', '4' ]);
+  });
+
+  it('deep find', () => {
+    const DeepFound = props => (<div>{ Children.deepFind(props.children, child => child.type === 'i') }</div>);
+    DeepFound.propTypes = { children: PropTypes.node };
+    const wrapper = shallow(<DeepFound><b>1</b><b>2</b><span><b>3</b></span><i>4</i></DeepFound>);
+    expect(wrapper).to.have.exactly(1).descendants('i');
+    expect(wrapper).to.have.text('4');
+  });
+
+  it('only text', () => {
+    const OnlyText = props => (<div>{ Children.onlyText(props.children) }</div>);
+    OnlyText.propTypes = { children: PropTypes.node };
+    const wrapper = shallow(
+      <OnlyText><span>0</span><b>1</b><span><i>2</i></span><i>3</i></OnlyText>
+    );
+    expect(wrapper).to.not.have.descendants('i');
+    expect(wrapper).to.not.have.descendants('b');
+    expect(wrapper).to.not.have.descendants('span');
+    expect(wrapper).to.have.text('0123');
+  });
+});
