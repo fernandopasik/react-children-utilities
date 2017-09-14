6cbb6a2c26486d458088224f7160a09703a31deb
yosef <maulerik@gmail.com>
2017-09-14 04:16:02 +0300

    added typescript typings

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import { ReactElement, ReactNode} from 'react';
+
+declare module 'react-children-utilities' {
+
+  export interface IKeyChildren {
+    [name: string]: ReactNode;
+  }
+
+  export interface ICallback {
+    (child: ReactElement<any>, index?: number): any
+  }
+
+  export function filter(children: ReactNode, filterFn: ICallback): ReactNode
+
+  export function groupByType(children: ReactNode, types: string[], rest?: string): IKeyChildren
+
+  export function deepMap(children: ReactNode, deepMapFn: ICallback): ReactNode
+
+  export function deepForEach(children: ReactNode, deepForEachFn: ICallback): void
+
+  export function deepFind(children: ReactNode, deepFindFn: ICallback): ReactElement<any>
+
+  export function onlyText(children: ReactNode): string
+}
