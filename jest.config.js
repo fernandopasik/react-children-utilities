1d5e07c48e51f4758758800978aef093f86ebeb3
Fernando Pasik <fernando@pasik.com.ar>
2019-01-06 20:43:22 +0000

    test: disable jsdom in run

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   setupFiles: [
     require.resolve('raf/polyfill'),
   ],
+  testEnvironment: 'node',
   setupTestFrameworkScriptFile: './node_modules/jest-enzyme/lib/index.js',
 };
