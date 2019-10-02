3812d64544a30055445671dfe92fe4be390f73e4
Fernando Pasik <fernando@pasik.com.ar>
2019-10-02 09:14:11 +0100

    test: remove duplicated tests

--- src/__tests__/index.spec.jsx
+++ src/__tests__/index.spec.jsx
@@ -21,29 +21,6 @@ describe('children', () => {
     expect(Children.deepFilter).toStrictEqual(deepFilter);
   });
 
-  it('deep filter', () => {
-    const DeepFiltered = ({ children }) => (
-      <div>{deepFilter(children, (item) => item.type === 'span')}</div>
-    );
-
-    const wrapper = shallow(
-      <DeepFiltered>
-        <span>1</span>
-        <span>2</span>
-        <span>
-          <strong>3</strong>
-          <span>
-            <strong>4</strong>
-            <span>5</span>
-          </span>
-        </span>
-      </DeepFiltered>,
-    );
-    expect(wrapper.find('span')).toExist();
-    expect(wrapper.find('span')).toHaveLength(5);
-    expect(wrapper.find('strong')).not.toExist();
-  });
-
   it('group by type', () => {
     const Grouped = ({ children }) => (
       <div>
