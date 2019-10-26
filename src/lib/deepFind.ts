ca201ded9dd6861901f9df22967629041e9d1e1c
Fernando Pasik <fernando@pasik.com.ar>
2019-10-26 18:47:58 +0100

    style: leave more empty lines

--- src/lib/deepFind.ts
+++ src/lib/deepFind.ts
@@ -7,18 +7,22 @@ export interface FindFunction {
 
 const deepFind = (children: ReactNode, deepFindFn: FindFunction): ReactNode | undefined => {
   let found;
+
   Children.toArray(children).find((child: ReactNode) => {
     if (hasComplexChildren(child)) {
       // Find inside the child that has children
       found = deepFind((child as ReactElement).props.children, deepFindFn);
       return typeof found !== 'undefined';
     }
+
     if (deepFindFn(child)) {
       found = child;
       return true;
     }
+
     return false;
   });
+
   return found;
 };
 
