852b708b5ad3a24d1fe20e3b3fef26a74c872784
Fernando Pasik <fernando@pasik.com.ar>
2020-02-25 23:57:51 +0000

    chore: update transform pattern in jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.{j,t}s{,x}'],
   testEnvironment: 'enzyme',
-  transform: { '^.+\\.[t|j]sx?$': 'ts-jest' },
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.all.json',
     },
   },
   setupFilesAfterEnv: ['./setupTests.ts'],
+  transform: { '^.+\\.[j|t]sx?$': 'ts-jest' },
 };
