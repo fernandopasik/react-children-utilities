db8aadbf9a3c68c751d5eedaaf05d10e49541a35
Fernando Pasik <fernando@pasik.com.ar>
2020-02-26 01:02:55 +0000

    style: run prettier on config file

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,7 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.{j,t}s{,x}'],
   testEnvironment: 'enzyme',
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.all.json',
-    },
-  },
+  globals: { 'ts-jest': { tsConfig: 'tsconfig.all.json' } },
   setupFilesAfterEnv: ['./setupTests.ts'],
   transform: { '^.+\\.[j|t]sx?$': 'ts-jest' },
 };
