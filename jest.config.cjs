bfd115e8e0dc9995dd8550215e7d893702b54d73
Fernando Pasik <fernando@pasik.com.ar>
2020-03-06 22:02:25 +0000

    chore: avoid setup tests file

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,6 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{j,t}s{,x}'],
   testEnvironment: 'enzyme',
   globals: { 'ts-jest': { tsConfig: 'tsconfig.all.json' } },
-  setupFilesAfterEnv: ['./setupTests.ts'],
+  setupFilesAfterEnv: ['jest-enzyme'],
   transform: { '^.+\\.[j|t]sx?$': 'ts-jest' },
 };
