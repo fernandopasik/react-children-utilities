8a682987bec4cde39527a349f0f2042289765ddf
Fernando Pasik <fernando@pasik.com.ar>
2020-03-23 22:46:12 +0000

    style: fix readonly parameters errors

--- src/lib/groupByType.ts
+++ src/lib/groupByType.ts
@@ -6,26 +6,29 @@ export interface GroupedChildren {
 
 const groupByType = (
   children: ReactNode,
-  types: ReactNode[] = [],
+  types: readonly ReactNode[] = [],
   // eslint-disable-next-line @typescript-eslint/no-inferrable-types
   rest: string = 'rest',
 ): GroupedChildren => {
-  return Children.toArray(children).reduce((groups: GroupedChildren, child: ReactNode) => {
-    const newGroups = { ...groups };
-    let key = rest;
+  return Children.toArray(children).reduce(
+    (groups: Readonly<GroupedChildren>, child: ReactNode) => {
+      const newGroups = { ...groups };
+      let key = rest;
 
-    if (isValidElement(child) && typeof child.type === 'string' && types.includes(child.type)) {
-      key = child.type;
-    }
+      if (isValidElement(child) && typeof child.type === 'string' && types.includes(child.type)) {
+        key = child.type;
+      }
 
-    if (typeof newGroups[key] === 'undefined') {
-      newGroups[key] = [];
-    }
+      if (typeof newGroups[key] === 'undefined') {
+        newGroups[key] = [];
+      }
 
-    newGroups[key] = [...newGroups[key], child];
+      newGroups[key] = [...newGroups[key], child];
 
-    return newGroups;
-  }, {});
+      return newGroups;
+    },
+    {},
+  );
 };
 
 export default groupByType;
