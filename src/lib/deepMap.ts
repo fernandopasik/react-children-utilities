1a7636d8647ea96343c6051dd654e438cbc45302
Fernando Pasik <fernando@pasik.com.ar>
2020-04-02 21:09:41 +0100

    refactor: convert to array before map

--- src/lib/deepMap.ts
+++ src/lib/deepMap.ts
@@ -3,8 +3,8 @@ import hasComplexChildren from './hasComplexChildren';
 
 export type MapFunction = (child: ReactNode, index?: number, children?: ReactNode[]) => ReactNode;
 
-const deepMap = (children: ReactNode, deepMapFn: MapFunction): ReactNode => {
-  return Children.map(children, (child: ReactNode) => {
+const deepMap = (children: ReactNode, deepMapFn: MapFunction): ReactNode[] => {
+  return Children.toArray(children).map((child: ReactNode) => {
     if (isValidElement(child) && hasComplexChildren(child)) {
       // Clone the child that has children and map them too
       return deepMapFn(
