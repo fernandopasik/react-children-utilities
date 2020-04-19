d65fff21b4fae873f539207303bb28461ce60fcf
Daniel Pinyol <dpinol@gmail.com>
2020-04-19 21:30:42 +0200

    fix: pass index & children to deepMap, deepForeach & deepFind (#17)

--- src/lib/deepFind.ts
+++ src/lib/deepFind.ts
@@ -1,25 +1,31 @@
 import { Children, isValidElement, ReactNode } from 'react';
 import hasComplexChildren from './hasComplexChildren';
 
-export type FindFunction = (child: ReactNode, index?: number, children?: ReactNode[]) => boolean;
+export type FindFunction = (
+  child: ReactNode,
+  index?: number,
+  children?: readonly ReactNode[],
+) => boolean;
 
 const deepFind = (children: ReactNode, deepFindFn: FindFunction): ReactNode | undefined => {
   let found;
 
-  Children.toArray(children).find((child: ReactNode) => {
-    if (deepFindFn(child)) {
-      found = child;
-      return true;
-    }
+  Children.toArray(children).find(
+    (child: ReactNode, index: number, findChildren: readonly ReactNode[]) => {
+      if (deepFindFn(child, index, findChildren)) {
+        found = child;
+        return true;
+      }
 
-    if (isValidElement(child) && hasComplexChildren(child)) {
-      // Find inside the child that has children
-      found = deepFind(child.props.children, deepFindFn);
-      return typeof found !== 'undefined';
-    }
+      if (isValidElement(child) && hasComplexChildren(child)) {
+        // Find inside the child that has children
+        found = deepFind(child.props.children, deepFindFn);
+        return typeof found !== 'undefined';
+      }
 
-    return false;
-  });
+      return false;
+    },
+  );
 
   return found;
 };
