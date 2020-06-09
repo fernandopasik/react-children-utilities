83386ce671bc44842fff40f9c411437ad707ffdc
Fernando Pasik <fernando@pasik.com.ar>
2020-06-09 23:35:55 +0100

    test: migrate jest config to es modules

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   collectCoverageFrom: ['src/**/*.{j,t}s{,x}'],
   globals: { 'ts-jest': { tsConfig: 'tsconfig.all.json' } },
   setupFilesAfterEnv: ['jest-enzyme'],
