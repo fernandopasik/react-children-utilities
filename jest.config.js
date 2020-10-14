bed99e93c18c012fb35d3bb4181809e5e983c2df
Fernando Pasik <fernando@pasik.com.ar>
2020-10-14 03:11:36 +0100

    test: update ts-jest tsconfig parameter name

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export default {
   collectCoverageFrom: ['src/**/*.{j,t}s{,x}'],
-  globals: { 'ts-jest': { tsConfig: 'tsconfig.all.json' } },
+  globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json' } },
   setupFilesAfterEnv: ['jest-enzyme'],
   testEnvironment: 'enzyme',
   transform: { '^.+\\.[j|t]sx?$': 'ts-jest' },
