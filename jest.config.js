53033a0673468cb3e415b98fc75e99573e264915
Fernando Pasik <fernando@pasik.com.ar>
2021-03-11 21:23:44 +0000

    style: sort props in config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 export default {
   collectCoverageFrom: ['src/**/*.{j,t}s{,x}'],
   globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json' } },
+  moduleNameMapper: { '(.*)\\.js': '$1' },
   setupFilesAfterEnv: ['jest-enzyme'],
   testEnvironment: 'enzyme',
   transform: { '^.+\\.[j|t]sx?$': 'ts-jest' },
-  moduleNameMapper: { '(.*)\\.js': '$1' },
 };
