506ef7c8612cfd7c3cc2250850d104cb185773b0
Fernando Pasik <fernando@pasik.com.ar>
2021-07-10 01:20:15 +0100

    test: set env in config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ export default {
   collectCoverageFrom: ['src/**/*.{j,t}s{,x}'],
   globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json' } },
   moduleNameMapper: { '(.*)\\.js': '$1' },
+  testEnvironment: 'node',
   transform: { '^.+\\.[j|t]sx?$': 'ts-jest' },
 };
