32802f449543b6440e5515b89c07aee485ad04da
Fernando Pasik <fernando@pasik.com.ar>
2021-07-10 17:39:30 +0100

    test: remove enzyme configs and dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,5 @@ export default {
   collectCoverageFrom: ['src/**/*.{j,t}s{,x}'],
   globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json' } },
   moduleNameMapper: { '(.*)\\.js': '$1' },
-  setupFilesAfterEnv: ['jest-enzyme'],
-  testEnvironment: 'enzyme',
   transform: { '^.+\\.[j|t]sx?$': 'ts-jest' },
 };
