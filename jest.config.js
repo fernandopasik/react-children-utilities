2bb605c7a6d900eefb1e8dba50c530cbed292bbd
fernandopasik <fernando@pasik.com.ar>
2022-01-01 19:41:17 +0000

    build: migrate to tsconfig build

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export default {
   collectCoverageFrom: ['src/**/*.{j,t}s{,x}'],
-  globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json', useESM: true } },
+  globals: { 'ts-jest': { useESM: true } },
   moduleNameMapper: { '(.*)\\.js': '$1' },
   preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
