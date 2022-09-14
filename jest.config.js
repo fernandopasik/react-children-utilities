85b2292daa1ef73c2297278e9d4ea47bde96bed4
fernandopasik <fernando@pasik.com.ar>
2022-09-14 13:23:15 +0100

    test: update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@
 
 export default {
   collectCoverageFrom: ['src/**/*.{j,t}s{,x}'],
-  globals: { 'ts-jest': { useESM: true } },
   moduleNameMapper: { '(.*)\\.js': '$1' },
   preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
