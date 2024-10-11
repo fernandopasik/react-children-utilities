b115574d40db2e4160d8db5ff39261454caa365e
Fernando Pasik <fernando@pasik.com.ar>
2024-10-11 18:35:31 +0100

    style: sort property

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,13 @@ import terser from '@rollup/plugin-terser';
 
 export default [
   {
+    external: ['react'],
     input: 'react-children-utilities.js',
     output: {
       file: 'react-children-utilities.min.js',
       format: 'esm',
       sourcemap: true,
     },
-    external: ['react'],
     plugins: [
       terser({
         mangle: {
